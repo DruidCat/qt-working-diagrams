dd6935b82c7a26fde1d64ebe708a985d7d836534
DruidCat <druidcat@yandex.ru>
2025-06-09 00:12:34 +0500

    Каталоги, создал класс многопоточного копирования файлов для каталога с обвязков в классе.

---
+++ src/copykatalog.cpp
@@ -0,0 +1,32 @@
+﻿#include "copykatalog.h"
+
+CopyKatalog::CopyKatalog(){
+///////////////////////////////
+//---К О Н С Т Р У К Т О Р---//
+///////////////////////////////
+    m_strAbsolutPutFaila = "";
+    m_strWorkingDiagramsPutFaila = "";
+}
+
+CopyKatalog::~CopyKatalog(){
+/////////////////////////////
+//---Д Е С Т Р У К Т О Р---//
+/////////////////////////////
+
+}
+
+void CopyKatalog::run(){//Перегружаемый метод, который копируем файл в потоке.
+/////////////////////////////////////////////////////
+//---К О П И Р У Е М   Ф А Й Л   В   П О Т О К Е---//
+/////////////////////////////////////////////////////
+    QFile flDannie (m_strAbsolutPutFaila);//Файл, который мы хотим скопировать, расположенный...
+    emit signalCopyDannie(flDannie.copy(m_strWorkingDiagramsPutFaila));//файл скопировался или нет
+}
+
+void CopyKatalog::ustPutiFailov(QString strAbsolutPutFaila, QString strWorkingDiagramsPutFaila){
+/////////////////////////////////////////////////////
+//---У С Т А Н О В И Т Ь   П У Т И   Ф А Й Л О В---//
+/////////////////////////////////////////////////////
+    m_strAbsolutPutFaila = strAbsolutPutFaila;
+    m_strWorkingDiagramsPutFaila = strWorkingDiagramsPutFaila;
+}
---
+++ src/copykatalog.h
@@ -0,0 +1,24 @@
+﻿#ifndef COPYKATALOG_H
+#define COPYKATALOG_H
+
+#include <QObject>
+#include <QThread>
+#include <QFile>
+
+class CopyKatalog: public QThread{
+    Q_OBJECT
+public:
+    explicit	CopyKatalog();//Конструктор
+    ~			CopyKatalog();//Деструктор
+    void  		run();//Перегруженый метод, копирующий файл.
+    void 		ustPutiFailov(QString strAbsolutPutFaila, QString strWorkingDiagramsPutFaila);
+private:
+    QString 	m_strAbsolutPutFaila;//Путь и сам копируемый файл.
+    QString 	m_strWorkingDiagramsPutFaila;//Имя и путь, куда копируется файл.
+
+signals:
+    void  		signalCopyDannie(bool);//Сигнал об окончании копирования. true -успех, false - ошибка.
+
+};
+
+#endif // COPYKATALOG_H
